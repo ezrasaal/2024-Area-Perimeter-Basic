50487ed019d536d2167e35eaefbfd525223a077f
ezrasaal <saale3653@masseyhigh.school.nz>
2024-08-23 14:47:59 +1200

    Add files via upload

---
+++ p_02_variables.py
@@ -0,0 +1,17 @@
+# code to learn about variables 
+
+# variables can be a string (note the speech marks)
+greeting = "hello world"
+
+
+# variables can be a number
+fav_num = 7
+num_string = "5"
+
+# constants are uppercase
+PI = 3.14
+
+a_01_my_num = 7
+
+print(3 * fav_num)
+print(3 * num_string)
